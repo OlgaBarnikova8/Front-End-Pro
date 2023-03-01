0552316c01e8344979dfb43493d88f67cccfca97
Olga Barnikova <barnikovaolga8@gmail.com>
2023-03-01 13:30:21 +0200

    added regExp function isValidDateFormat(str)

--- homework_14/script.js
+++ homework_14/script.js
@@ -28,6 +28,22 @@ function setUserInlocalStorage() {
 setUserInlocalStorage();
 
 
+// 2. Написать функцию isValidDateFormat(str) , которая бы проверяла 
+// валидный формат даты (YYYY/MM/DD HH:MM) или нет
+// Функция не должна проверять что дата оригинальна (например можно ввести 32 день месяца) - 
+// она должна проверять именно формат даты YYYY/MM/DD HH:MM) Для решения данной задачи используем регулярные выражения
+
+// Также для решения задачи вам поможет эта статья: https://uk.javascript.info/regexp-escaping
+
+function isValidDateFormat(str) {
+    const regexp = /^\d{4}\/\d{2}\/\d{2} \d{2}\:\d{2}$/;
+    return regexp.test(str); 
+}
+
+console.log(isValidDateFormat("2012/09/18 12:10")); // true
+console.log(isValidDateFormat("12.02.2022 12:10")); // false
+
+
 // 3. Написать функцию isAfter , которая будет принимать 2 аргумента даты 
 // (это может быть либо объект Date, либо timestamp). 
 // Функция должна вернуть true, если первая переданная дата будет позже, чем вторая. 
@@ -45,7 +61,6 @@ function isAfter(date1, date2) {
 
     return true;
 }
-
 
 console.log(isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11))); // true
 console.log(isAfter(1648636135000, 1648549735000)); // true
