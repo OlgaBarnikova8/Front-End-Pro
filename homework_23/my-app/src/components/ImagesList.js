04f1c1aaa7d6d9082170797971584838fb7512ab
Olga Barnikova <barnikovaolga8@gmail.com>
2023-03-27 16:43:33 +0300

    added React API images

---
+++ homework_23/my-app/src/components/ImagesList.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+function getPhotos(page = 1, limit = 10) {
+    return fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
+      .then((response) => response.json());
+  }
+
+const ImagesList = () => {
+    const [page, setPage] = useState(1);
+    const [images, setImages] = useState([]);
+    const [isLoading, setLoading] = useState(true);    
+    
+    useEffect(() => {  
+        getPhotos(page)                    
+          .then(data => {        
+            setImages((prevValue) => [...prevValue, ...data]);           
+            setLoading(false);  
+          })      
+    }, [page]);
+    
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
+    const handleClick = () => {
+        setLoading(true);
+        setPage((prevValue) => ++prevValue);
+    };
+
+    return (
+        <>
+          <div className="container__photos">
+            {images.map(({id, url, author: alt}) => (          
+            <img key= {id} src={url} alt={alt} className="photos" />  
+            ))}            
+        </div>
+        <button onClick={handleClick} className="btn">Show more</button>       
+        </>    
+    )
+};
+
+export default ImagesList;
